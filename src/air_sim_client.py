correct translattion, need to get correct rotation

src/air_sim_client.py
@@ -52,11 +52,7 @@ class AirSimClient:
         raw_img = self.client.simGetImages([airsim.ImageRequest("0", airsim.ImageType.DepthPlanner, True, False)])[0]
         pose = self.client.getMultirotorState()
         self.client.simPause(False)
-
-        # dpeth_img_meters = airsim.get_pfm_array(raw_img).reshape(raw_img.height, raw_img.width, 1)
-        # depth_8bit_lerped = np.interp(dpeth_img_meters, (0, 150), (0, 255)).astype('uint8')
-
-        return airsim.get_pfm_array(raw_img), pose
+        return airsim.get_pfm_array(raw_img).reshape(raw_img.height, raw_img.width, 1), (pose.rc_data.roll, pose.rc_data.pitch, pose.rc_data.yaw)
 
     def flyToPosAndYaw(self, pos_to_fly, yaw):
         pos = airsim.Vector3r(pos_to_fly[0], pos_to_fly[1], -pos_to_fly[2])

src/depth_image_processor.py
@@ -3,6 +3,7 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import open3d as o3d
+from scipy.spatial.transform import Rotation as R
 
 class depthImageProcessor():
     def __init__(self):
@@ -17,30 +18,47 @@ class depthImageProcessor():
         fy = fx
 
         intrinsic_params = o3d.camera.PinholeCameraIntrinsic(width, height, fx, fy, cx, cy)
-        # extrinsic = np.array([[1., 0., 0., 0.], 
-        #                       [0., 1., 0., 0.], 
-        #                       [0., 0., 1., 0.], 
-        #                       [0., 0., 0., 1.]])
+        extrinsic = np.array([[1, 0., 0., 0.], 
+                              [0., 1., 0., 0.], 
+                              [0., 0., 1., 0.], 
+                              [0., 0., 0., 1.]])
 
         depth_img = np.where(depth_img > 150, 0, depth_img)
         img = o3d.geometry.Image(depth_img)
 
-        pcd = o3d.geometry.PointCloud.create_from_depth_image(img, intrinsic_params)
+        pcd = o3d.geometry.PointCloud.create_from_depth_image(img, intrinsic_params, extrinsic)
         pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+        pcd.translate((cam_pose[0][1], cam_pose[0][0], cam_pose[0][2]))
+        r = R.from_euler('x', 90, degrees=True)
+        pcd.rotate(r.as_matrix(), center=[cam_pose[0][1], cam_pose[0][0], cam_pose[0][2]])
+        r2 = R.from_euler('z', 360-cam_pose[1][2], degrees=True)
+        pcd.rotate(r2.as_matrix(), center=[cam_pose[0][1], cam_pose[0][0], cam_pose[0][2]])
 
-        bbox = pcd.get_axis_aligned_bounding_box()
-        axes = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1, origin=[0, 0, 0])
+        voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd, voxel_size=1)
 
-        o3d.visualization.draw_geometries([pcd, bbox, axes])
+        bbox = voxel_grid.get_axis_aligned_bounding_box()
+
+        axes = o3d.geometry.TriangleMesh.create_coordinate_frame(size=10, origin=[0, 0, 0])
+
+        sphere = o3d.geometry.TriangleMesh.create_sphere(1)
+        sphere.translate((cam_pose[0][1], cam_pose[0][0], cam_pose[0][2]))
+
+        voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd, voxel_size=1)
+
+        vis = o3d.visualization.Visualizer()
+        vis.create_window()
+        vis.add_geometry(pcd)
+        vis.add_geometry(axes)
+        vis.add_geometry(sphere)
+
+        for i in range(360):
+            r3 = R.from_euler('z', 1, degrees=True)
+            pcd.rotate(r3.as_matrix(), center=[cam_pose[0][1], cam_pose[0][0], cam_pose[0][2]])
+
+            vis.update_geometry(pcd)
+
+            vis.poll_events()
+            vis.update_renderer()
 
 
-        # pcd_cam = o3d.geometry.PointCloud()
-        # pcd_cam.points = o3d.utility.Vector3dVector(cam_coords.T[:, :3])
-        # pcd_cam.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-
-        # bbox = pcd_cam.get_axis_aligned_bounding_box()
-        # axes = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1, origin=[0, 0, 0])
-
-        # voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd_cam, voxel_size=1)
-        # o3d.visualization.draw_geometries([voxel_grid, bbox, axes])
-
+        o3d.visualization.draw_geometries([voxel_grid, axes, sphere])